4937d7c9de73cb4a4a1c377d1b7385f280c00fa2
Thomas <wyatsky@gmail.com>
2019-12-19 08:45:59 +1000

    Remove commented out mu4e sections that were used for single account.

--- init.el
+++ init.el
@@ -410,16 +410,6 @@
 ;; additional non-Gmail addresses and want assign them different
 ;; behavior.)
 
-;; setup some handy shortcuts
-;; you can quickly switch to your Inbox -- press ``ji''
-;; then, when you want archive some messages, move them to
-;; the 'All Mail' folder by pressing ``ma''.
-
-; (setq mu4e-maildir-shortcuts
-;     '( ("/INBOX"               . ?i)
-;        ("/[Gmail].Sent Mail"   . ?s)
-;        ("/[Gmail].Trash"       . ?t)
-;        ("/[Gmail].All Mail"    . ?a)))
 
 ;; allow for updating mail using 'U' in the main view:
 ;; (setq mu4e-get-mail-command "offlineimap")
@@ -479,36 +469,7 @@
 
 ;; don't ask when quitting
 (setq mu4e-confirm-quit nil)
-
-;; something about ourselves
-; (setq
-;    user-mail-address "wyatsky@gmail.com"
-;    user-full-name  "Thomas Wang"
-;    mu4e-compose-signature
-;     (concat
-;       "Thomas\n"
-;       "http://www.yaoni.xyz\n"))
-
-;; sending mail -- replace USERNAME with your gmail username
-;; also, make sure the gnutls command line utils are installed
-;; package 'gnutls-bin' in Debian/Ubuntu
-
 (require 'smtpmail)
-; (setq message-send-mail-function 'smtpmail-send-it
-;    starttls-use-gnutls t
-;    smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-;    smtpmail-auth-credentials
-;      '(("smtp.gmail.com" 587 "wyatsky@gmail.com" nil))
-;    smtpmail-default-smtp-server "smtp.gmail.com"
-;    smtpmail-smtp-server "smtp.gmail.com"
-;    smtpmail-smtp-service 587)
-
-;; alternatively, for emacs-24 you can use:
-;;(setq message-send-mail-function 'smtpmail-send-it
-;;     smtpmail-stream-type 'starttls
-;;     smtpmail-default-smtp-server "smtp.gmail.com"
-;;     smtpmail-smtp-server "smtp.gmail.com"
-;;     smtpmail-smtp-service 587)
 
 ;; don't keep message buffers around
 (setq message-kill-buffer-on-exit t)
