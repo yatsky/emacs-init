7cfdd623dde53e680f6ede6827b5ceb15428be90
Thomas <wyatsky@gmail.com>
2019-12-18 21:19:51 +1000

    Comment out settings for a single account. Get ready for mu4e-context

--- init.el
+++ init.el
@@ -399,9 +399,9 @@
 ;; default
 (setq mu4e-maildir "~/.mail/my-gmail")
 
-(setq mu4e-drafts-folder "/[Gmail].Drafts")
-(setq mu4e-sent-folder   "/[Gmail].Sent Mail")
-(setq mu4e-trash-folder  "/[Gmail].Trash")
+; (setq mu4e-drafts-folder "/[Gmail].Drafts")
+; (setq mu4e-sent-folder   "/[Gmail].Sent Mail")
+; (setq mu4e-trash-folder  "/[Gmail].Trash")
 
 ;; don't save message to Sent Messages, Gmail/IMAP takes care of this
 (setq mu4e-sent-messages-behavior 'delete)
@@ -415,11 +415,11 @@
 ;; then, when you want archive some messages, move them to
 ;; the 'All Mail' folder by pressing ``ma''.
 
-(setq mu4e-maildir-shortcuts
-    '( ("/INBOX"               . ?i)
-       ("/[Gmail].Sent Mail"   . ?s)
-       ("/[Gmail].Trash"       . ?t)
-       ("/[Gmail].All Mail"    . ?a)))
+; (setq mu4e-maildir-shortcuts
+;     '( ("/INBOX"               . ?i)
+;        ("/[Gmail].Sent Mail"   . ?s)
+;        ("/[Gmail].Trash"       . ?t)
+;        ("/[Gmail].All Mail"    . ?a)))
 
 ;; allow for updating mail using 'U' in the main view:
 ;; (setq mu4e-get-mail-command "offlineimap")
@@ -479,27 +479,27 @@
 (setq mu4e-confirm-quit nil)
 
 ;; something about ourselves
-(setq
-   user-mail-address "wyatsky@gmail.com"
-   user-full-name  "Thomas Wang"
-   mu4e-compose-signature
-    (concat
-      "Thomas\n"
-      "http://www.yaoni.xyz\n"))
+; (setq
+;    user-mail-address "wyatsky@gmail.com"
+;    user-full-name  "Thomas Wang"
+;    mu4e-compose-signature
+;     (concat
+;       "Thomas\n"
+;       "http://www.yaoni.xyz\n"))
 
 ;; sending mail -- replace USERNAME with your gmail username
 ;; also, make sure the gnutls command line utils are installed
 ;; package 'gnutls-bin' in Debian/Ubuntu
 
 (require 'smtpmail)
-(setq message-send-mail-function 'smtpmail-send-it
-   starttls-use-gnutls t
-   smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-   smtpmail-auth-credentials
-     '(("smtp.gmail.com" 587 "wyatsky@gmail.com" nil))
-   smtpmail-default-smtp-server "smtp.gmail.com"
-   smtpmail-smtp-server "smtp.gmail.com"
-   smtpmail-smtp-service 587)
+; (setq message-send-mail-function 'smtpmail-send-it
+;    starttls-use-gnutls t
+;    smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+;    smtpmail-auth-credentials
+;      '(("smtp.gmail.com" 587 "wyatsky@gmail.com" nil))
+;    smtpmail-default-smtp-server "smtp.gmail.com"
+;    smtpmail-smtp-server "smtp.gmail.com"
+;    smtpmail-smtp-service 587)
 
 ;; alternatively, for emacs-24 you can use:
 ;;(setq message-send-mail-function 'smtpmail-send-it
