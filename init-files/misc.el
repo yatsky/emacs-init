3f677c8fa72181c30364ee86523b40f260ef9e33
Thomas <wyatsky@gmail.com>
2020-01-16 14:37:44 +1000

    Add pyim and posframe

--- init-files/misc.el
+++ init-files/misc.el
@@ -44,6 +44,29 @@
 ;; 让 spacemacs mode-line 中的 Unicode 图标正确显示。
 ;; (cnfonts-set-spacemacs-fallback-fonts)
 
+;; pyim
+(require 'posframe)
+(require 'pyim)
+(require 'pyim-basedict)
+(pyim-basedict-enable)
+(setq default-input-method "pyim")
+(global-set-key (kbd "C-\\") 'toggle-input-method)
+;; 使用 popup-el 来绘制选词框, 如果用 emacs26, 建议设置
+;; 为 'posframe, 速度很快并且菜单不会变形，不过需要用户
+;; 手动安装 posframe 包。
+(setq pyim-page-tooltip 'posframe)
+;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
+;; 我自己使用的中英文动态切换规则是：
+;; 1. 光标只有在注释里面时，才可以输入中文。
+;; 2. 光标前是汉字字符时，才能输入中文。
+;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
+(setq-default pyim-english-input-switch-functions
+            '(pyim-probe-dynamic-english
+                pyim-probe-isearch-mode
+                pyim-probe-program-mode
+                pyim-probe-org-structure-template))
+(global-set-key (kbd"M-j") 'pyim-convert-string-at-point) ;与 pyim-probe-dynamic-english 配合
+(global-set-key (kbd"C-;") 'pyim-delete-word-from-personal-buffer)
 ;; set magit global keybinding
 (global-set-key (kbd "C-x g") 'magit-status)
 
