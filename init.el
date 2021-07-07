58dbc7004869b460ab70194f2b8c55d60d406948
Yi Wang <yi@customology.com.au>
2021-07-08 08:43:44 +1000

    Install more software and update settings.

--- init.el
+++ init.el
@@ -23,7 +23,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("947190b4f17f78c39b0ab1ea95b1e6097cc9202d55c73a702395fc817f899393" "b46ee2c193e350d07529fcd50948ca54ad3b38446dcbd9b28d0378792db5c088" default))
+   '("691421cecade32cf48772346303b19ea5a1af275122c1593c631e6d846549dd0" "81c3de64d684e23455236abde277cda4b66509ef2c28f66e059aa925b8b12534" "947190b4f17f78c39b0ab1ea95b1e6097cc9202d55c73a702395fc817f899393" "b46ee2c193e350d07529fcd50948ca54ad3b38446dcbd9b28d0378792db5c088" default))
  '(debug-on-error t)
  '(ein:completion-backend 'ein:use-none-backend)
  '(ein:output-area-inlined-images t)
@@ -60,12 +60,14 @@
  '(org-refile-targets
    '((org-agenda-files :regexp . "*")
      (org-agenda-files :maxlevel . 5)))
+ '(org-roam-server-mode t)
  '(org-screenshot-image-directory "/tmp/images/")
  '(org-src-tab-acts-natively t)
  '(package-check-signature nil)
  '(package-selected-packages
-   '(xah-find ox-reveal nov org-noter blacken command-log-mode org-tree-slide emojify pyim-wbdict all-the-icons use-package helm-core virtualenvwrapper projectile pdf-tools posframe pyim json-mode elisp-format general gif-screencast which-key org-mime evil-collection flycheck tide web-mode rjsx-mode org-bullets magit org-plus-contrib el-get gnu-elpa-keyring-update jedi-core cnfonts ein request-deferred elfeed exec-path-from-shell indium htmlize auctex yasnippet-snippets jedi powerline dracula-theme evil-magit helm org-evil evil org undo-tree))
- '(pdf-view-midnight-colors '("white smoke" . "black")))
+   '(dired-sidebar dired-subtree xah-find ox-reveal nov org-noter blacken command-log-mode org-tree-slide emojify pyim-wbdict all-the-icons use-package helm-core virtualenvwrapper projectile pdf-tools posframe pyim json-mode elisp-format general gif-screencast which-key org-mime evil-collection flycheck tide web-mode rjsx-mode org-bullets magit org-plus-contrib el-get gnu-elpa-keyring-update jedi-core cnfonts ein request-deferred elfeed exec-path-from-shell indium htmlize auctex yasnippet-snippets jedi powerline dracula-theme evil-magit helm org-evil evil org undo-tree))
+ '(pdf-view-midnight-colors '("white smoke" . "black"))
+ '(smartparens-global-strict-mode t))
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/yaoni.org"))
