66bf6710c9a03f5230e973c82fa52861d8d33276
yaoni <y.wang7@uq.net.au>
2022-06-20 12:00:54 +1000

    change settings and fix bug in uuid generation code.

--- init.el
+++ init.el
@@ -34,30 +34,24 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
- '(ansi-term-color-vector
-   [unspecified "#FFFFFF" "#d15120" "#5f9411" "#d2ad00" "#6b82a7" "#a66bab" "#6b82a7" "#505050"])
  '(custom-safe-themes
-   '("0c75b3f8c27553bcc42af578d4d171fa49036aeb01dda8f149d632331002acd7" "ed8e6f452855fc7338c8be77803666b34745c19c6667197db48952107fa6d983" "03f28a4e25d3ce7e8826b0a67441826c744cbf47077fb5bc9ddb18afe115005f" "691421cecade32cf48772346303b19ea5a1af275122c1593c631e6d846549dd0" "81c3de64d684e23455236abde277cda4b66509ef2c28f66e059aa925b8b12534" "947190b4f17f78c39b0ab1ea95b1e6097cc9202d55c73a702395fc817f899393" "b46ee2c193e350d07529fcd50948ca54ad3b38446dcbd9b28d0378792db5c088" default))
+   '("ed8e6f452855fc7338c8be77803666b34745c19c6667197db48952107fa6d983" "03f28a4e25d3ce7e8826b0a67441826c744cbf47077fb5bc9ddb18afe115005f" "691421cecade32cf48772346303b19ea5a1af275122c1593c631e6d846549dd0" "81c3de64d684e23455236abde277cda4b66509ef2c28f66e059aa925b8b12534" "947190b4f17f78c39b0ab1ea95b1e6097cc9202d55c73a702395fc817f899393" "b46ee2c193e350d07529fcd50948ca54ad3b38446dcbd9b28d0378792db5c088" default))
  '(debug-on-error t)
  '(elfeed-feeds '("https://nyaa.si/?page=rss"))
- '(fci-rule-character-color "#d9d9d9")
- '(fci-rule-color "#d9d9d9")
  '(global-display-line-numbers-mode t)
  '(global-emojify-mode t)
  '(global-emojify-mode-line-mode t)
- '(load-prefer-newer t)
+ '(load-prefer-newer t t)
  '(org-agenda-search-headline-for-time nil)
  '(org-agenda-skip-additional-timestamps-same-entry t)
  '(org-agenda-skip-timestamp-if-deadline-is-shown nil)
  '(org-babel-load-languages
    '((js . t)
-	 (java . t)
-	 (python . t)
-	 (emacs-lisp . t)
-	 (shell . t)
-	 (ditaa . t)))
+     (java . t)
+     (python . t)
+     (emacs-lisp . t)
+     (shell . t)
+     (ditaa . t)))
  '(org-confirm-babel-evaluate nil)
  '(org-export-use-babel nil)
  '(org-html-htmlize-output-type 'css)
@@ -66,14 +60,15 @@
    '(org-bbdb org-bibtex org-docview org-eww org-gnus org-info org-irc org-mhe org-rmail org-w3m))
  '(org-refile-targets
    '((org-agenda-files :regexp . "*")
-	 (org-agenda-files :maxlevel . 5)))
+     (org-agenda-files :maxlevel . 5)))
  '(org-screenshot-image-directory "/tmp/images/")
  '(org-src-tab-acts-natively t)
  '(package-check-signature nil)
  '(package-selected-packages
    '(ox-jira dired-sidebar dired-subtree xah-find ox-reveal nov org-noter blacken command-log-mode org-tree-slide emojify pyim-wbdict all-the-icons use-package virtualenvwrapper pdf-tools posframe pyim json-mode elisp-format general gif-screencast which-key org-mime evil-collection flycheck tide web-mode rjsx-mode org-bullets magit el-get gnu-elpa-keyring-update jedi-core cnfonts request-deferred elfeed exec-path-from-shell htmlize auctex yasnippet-snippets jedi powerline dracula-theme evil-magit org-evil evil org undo-tree))
  '(pdf-view-midnight-colors '("white smoke" . "black"))
- '(smartparens-global-strict-mode t))
+ '(smartparens-global-strict-mode t)
+ '(xclip-program "powershell.exe"))
 
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/yaoni.org"))
